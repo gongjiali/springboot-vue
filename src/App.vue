ece29e8e39f2a0c57f1274e6057c59484dbbdcc5
gongjl <gongjl@shanghaitech.edu.cn>
2022-06-18 09:48:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header />
+    <div style="display: flex">
+      <Aside />
+    </div>
+  </div>
+  <router-view />
+</template>
+
+<style>
+</style>
+
+<script>
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+export default {
+  Name: "Layout",
+  components: {
+    Header,
+    Aside,
+  },
+  
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <span>
+      <el-input
+        v-model="search"
+        placeholder="Please input"
+        clearable
+        style="margin-left:20px; padding: 10px; width: 350px"
+      />
+    </span>
+    <span style="padding: 10px">
+      <el-button type="primary">search</el-button>
+    </span>
+  </div>
+
+  <div style="display: flex; margin-left: 50px; margin-top: 50px">
+    <el-table
+      :data="tableData"
+      :default-sort="{ prop: 'date', order: 'descending' }"
+    >
+      <el-table-column prop="date" label="Date" sortable />
+      <el-table-column prop="name" label="Name" />
+      <el-table-column prop="address" label="Address" :formatter="formatter" />
+    </el-table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {},
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+        {
+          date: "2016-05-02",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+        {
+          date: "2016-05-04",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+        {
+          date: "2016-05-01",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+      ],
+    };
+  },
+};
+</script>
