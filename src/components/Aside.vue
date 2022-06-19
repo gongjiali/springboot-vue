43d9bec33cca25378ee7b12b20245b99e6b3e0e5
gongjl <gongjl@shanghaitech.edu.cn>
2022-06-19 20:39:24 +0800

    init

---
+++ src/components/Aside.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-row class="tac" style="width:150px; display:flex">
+    <el-col :span="12">
+      <h5 class="mb-2"></h5>
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="#545c64"
+        class="el-menu-vertical-demo"
+        default-active="1"
+        text-color="#fff"
+        @open="handleOpen"
+        @close="handleClose"
+        style ="width: 150px; "
+      >
+        <el-menu-item index="1">
+          <el-icon><icon-menu /></el-icon>
+          <span>Authors</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <el-icon><icon-menu /></el-icon>
+          <span>Reviewers</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <el-icon><document /></el-icon>
+          <span>Articles</span>
+        </el-menu-item>
+
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+
+
+
+
+<script>
+export default {
+  name: "Aside",
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,64 @@
+<template>
+  <div
+    style="
+      height: 50px;
+      line-height: 50px;
+      border-bottom: 4px solid #ccc;
+      display: flex;
+      background-color: #545c64;
+    "
+  >
+    <div
+      style="width: 200px; padding-left: 30px; font-weight: bold; color: #fff"
+    >
+      Administrator
+    </div>
+    <div style="width: 835px;"> </div>
+
+    <div style="flex: 1; clear: both">
+      <el-dropdown>
+        <span
+          class="el-dropdown-link"
+          style="
+            padding-top: 17px;
+            font-weight: bold;
+            color: #fff;
+          "
+        >
+          Personal center
+          <el-icon class="el-icon--right">
+            <arrow-down />
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>Personal information</el-dropdown-item>
+            <el-dropdown-item>Change password</el-dropdown-item>
+            <!-- <el-dropdown-item>Action 3</el-dropdown-item>
+            <el-dropdown-item disabled>Action 4</el-dropdown-item>
+            <el-dropdown-item divided>Action 5</el-dropdown-item> -->
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+
+
+
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.example-showcase .el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+}
+</style>
